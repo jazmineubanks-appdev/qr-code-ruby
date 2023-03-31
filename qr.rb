d1c494275cac00adc280579a7363c59a5eef9422
Jazmin Eubanks <jazmin.eubanks@gmail.com>
2023-03-31 16:16:52 +0000

    qr code

--- qr.rb
+++ qr.rb
@@ -1,1 +1,6 @@
-"Hello World!"
+require 'rqrcode'
+qrcode = RQRCode::QRCode.new("https://jazmineubanks.github.io/jazmin-eubanks.github.io/")
+png = qrcode.as_png({ :size => 500 })
+IO.binwrite("sometext.png", png.to_s)
+
+puts ARGV
